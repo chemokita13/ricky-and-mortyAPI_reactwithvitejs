838c0e55820a7b9bf989bc161d498926ad5ef7a2
chemokita13 <chemokita13@gmail.com>
2022-12-11 13:30:30 +0100

    deploy error solves

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "https://github.io/chemokita13/ricky-and-mortyAPI_reactwithvitejs",
+    base: "https://chemokita13.github.io/ricky-and-mortyAPI_reactwithvitejs",
     plugins: [
         react({
             include: "./src/**/*.{js,ts,jsx,tsx}",
